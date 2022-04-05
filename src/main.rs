f75d0f288c87867497889ab87a4370d9022d35ad
Michael Christenson II <me@xorsense.nl>
2022-04-05 11:01:07 -0400

    Show how unit testing works

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
-    let msg = sprintln("Hello, world!");
-    println!("{}", msg);
-}
+fn main() {}
 
-fn sprintln(msg: &str) -> String {
-    format!("{}\n", msg)
+fn add(l: i64, r: i64) -> i64 {
+    l + r
+}
+/*
+   modules
+       | main
+           | tests
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_nums() {
+        let a = 2;
+        let b = 2;
+        let expected = a + b;
+        let got = add(a, b);
+        println!("a: {}, b: {}, expected: {}, got: {}", a, b, expected, got);
+        assert_eq!(expected, got);
+    }
 }
